4203c7fcd0745001556fa2b6e56a168007c4ee82
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-02-15 21:52:03 -0300

    mais organizado

--- balanço.py
+++ balanço.py
@@ -1,19 +1,23 @@
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 
 def get_valor_total(nome_categoria):
     valor_total = Decimal('0')
     while True:
-        valor = Decimal(input(f"Insira o valor da {nome_categoria}: "))
+        try:
+            valor = Decimal(input(f"Insira o valor da {nome_categoria}: "))
+        except InvalidOperation:
+            print("Valor inválido. Tente novamente.\n")
+            continue
         valor_total += valor
-        mais_valores = input(f"Deseja inserir outro valor de {nome_categoria}? \n1:SIM\n2:NÃO ")
+        print("\n\033[32mInserido com sucesso!\033[0m\n")
+        mais_valores = input(f"Deseja inserir outro valor de {nome_categoria}?\n1: SIM\n2: NÃO\n> ")
         if mais_valores.lower() == "1":
-            print("Valores atuais:")
-            print(f"{nome_categoria}: {valor_total}")
+            print(f"\nValores atuais de {nome_categoria}: {valor_total}\n")
         elif mais_valores.lower() == "2":
-            print(f"{nome_categoria} totais: {valor_total}")
+            print(f"\n{nome_categoria.title()} totais: {valor_total}\n")
             return valor_total
         else:
-            print("Resposta inválida. Tente novamente.")
+            print("Resposta inválida. Tente novamente.\n")
 
 ativos = get_valor_total("Ativo")
 passivos = get_valor_total("Passivo")
@@ -24,12 +28,12 @@ patrimonio_liquido = ativos - passivos
 resultado = receitas - despesas
 
 print("\nBalanço Patrimonial")
-print("Ativos: R$", patrimonio_liquido)
-print("Passivos: R$", passivos)
-print("Patrimônio Líquido: R$", patrimonio_liquido)
-print("Resultado do Exercício")
+print(f"Ativos: R$ {patrimonio_liquido:.2f}")
+print(f"Passivos: R$ {passivos:.2f}")
+print(f"Patrimônio Líquido: R$ {patrimonio_liquido:.2f}")
+print("Resultado do Exercício:")
 
 if resultado > 0:
-    print("Lucro: R$", resultado)
+    print(f"Lucro: R$ {resultado:.2f}")
 else:
-    print("Prejuízo: R$", resultado)
+    print(f"Prejuízo: R$ {resultado:.2f}")
