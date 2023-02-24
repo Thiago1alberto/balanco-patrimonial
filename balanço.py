e82220b125b367949d061ed48e0e492fe173d0b5
thiag0000 <thiagorio11@gmail.com>
2023-02-24 20:20:05 -0300

    consolidando a base

--- balanço.py
+++ balanço.py
@@ -1,111 +1,63 @@
 from decimal import Decimal, InvalidOperation
-import tkinter as tk
-from tkinter import simpledialog, messagebox
 
-class BalancoPatrimonialApp(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.master.title("Balanço Patrimonial")
-        self.master.geometry("400x300")
-        self.pack()
-        self.create_widgets()
+class BalancoPatrimonial:
+    CATEGORIA_ATIVO = "ativo"
+    CATEGORIA_PASSIVO = "passivo"
+    CATEGORIA_PATRIMONIO_LIQUIDO = "patrimônio líquido"
 
-    def create_widgets(self):
-        self.ativo_label = tk.Label(self, text="Ativos:")
-        self.ativo_label.pack(side="top")
-        self.ativo_valor = tk.Label(self, text="0.00")
-        self.ativo_valor.pack(side="top")
-        self.ativo_button = tk.Button(self, text="Adicionar valor de Ativo", command=self.add_ativo, width=30)
-        self.ativo_button.pack(side="top")
-
-        self.passivo_label = tk.Label(self, text="Passivos:")
-        self.passivo_label.pack(side="top")
-        self.passivo_valor = tk.Label(self, text="0.00")
-        self.passivo_valor.pack(side="top")
-        self.passivo_button = tk.Button(self, text="Adicionar valor de Passivo", command=self.add_passivo, width=30)
-        self.passivo_button.pack(side="top")
-
-        self.patrimonio_label = tk.Label(self, text="Patrimônio Líquido:")
-        self.patrimonio_label.pack(side="top")
-        self.patrimonio_valor = tk.Label(self, text="0.00")
-        self.patrimonio_valor.pack(side="top")
-        self.patrimonio_button = tk.Button(self, text="Adicionar valor de Patrimônio Líquido", command=self.add_patrimonio, width=30)
-        self.patrimonio_button.pack(side="top")
-
-        self.quit = tk.Button(self, text="Fechar", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def add_ativo(self):
-        descricao, valor = self.get_descricao_valor("Ativo")
-        atual = Decimal(self.ativo_valor["text"])
-        novo = atual + valor
-        self.ativo_valor["text"] = str(novo)
-
-    def add_passivo(self):
-        descricao, valor = self.get_descricao_valor("Passivo")
-        atual = Decimal(self.passivo_valor["text"])
-        novo = atual + valor
-        self.passivo_valor["text"] = str(novo)
-
-    def add_patrimonio(self):
-        descricao, valor = self.get_descricao_valor("Patrimônio Líquido")
-        atual = Decimal(self.patrimonio_valor["text"])
-        novo = atual + valor
-        self.patrimonio_valor["text"] = str(novo)
+    def __init__(self):
+        self.ativos = Decimal('0')
+        self.passivos = Decimal('0')
+        self.patrimonio_liquido = Decimal('0')
+        self.descricoes = {'ativos': [], 'passivos': [], 'patrimonio_liquido': []}
         
-    
-    def get_descricao_valor(self, categoria):
-        nova_janela = tk.Toplevel(self.master)
-        nova_janela.title(f"Adicionar {categoria}")
-        nova_janela.geometry("300x150")
-        nova_janela.resizable(False, False)
-
-        descricao_label = tk.Label(nova_janela, text="Descrição:")
-        descricao_label.pack(side="top", padx=10, pady=10)
-        descricao_entry = tk.Entry(nova_janela, width=30)
-        descricao_entry.pack(side="top", padx=10)
-
-        valor_label = tk.Label(nova_janela, text="Valor:")
-        valor_label.pack(side="top", padx=10, pady=10)
-        valor_entry = tk.Entry(nova_janela, width=10)
-        valor_entry.pack(side="top", padx=10)
-
-        confirmar_button = tk.Button(nova_janela, text="Confirmar", command=lambda: self.adicionar_descricao_valor(categoria, descricao_entry.get(), valor_entry.get(), nova_janela))
-        confirmar_button.pack(side="top", padx=10, pady=10)
-        confirmar_button.config(height=10)
-        
-    def adicionar_descricao_valor(self, categoria, descricao, valor, nova_janela):
-        try:
-            valor_decimal = Decimal(valor)
-        except InvalidOperation:
-            tk.messagebox.showerror("Valor inválido", "Valor inválido. Tente novamente.")
-            return
-        atual = Decimal(getattr(self, f"{categoria.lower()}_valor")["text"])
-        novo = atual + valor_decimal
-        getattr(self, f"{categoria.lower()}_valor")["text"] = str(novo)
-
-        nova_janela.destroy()
-
-    def get_valor(self, categoria):
+    def get_valor_total(self, nome_categoria):
+        itens = []
+        valor_total = Decimal('0')
         while True:
-            valor_str = tk.simpledialog.askstring("Inserir valor", f"Insira o valor de {categoria}:")
+            descricao = input(f"Insira a descrição do item de {nome_categoria}: ")
+            valor_str = input(f"Insira o valor do item de {nome_categoria} '{descricao}': ")
             try:
                 valor = Decimal(valor_str)
-                return valor
             except InvalidOperation:
-                tk.messagebox.showerror("Valor inválido", "Valor inválido. Tente novamente.")
-                
-    def calcular_balanco(self):
-        ativo = Decimal(self.ativo_valor["text"])
-        passivo = Decimal(self.passivo_valor["text"])
-        patrimonio = Decimal(self.patrimonio_valor["text"])
-        if ativo == passivo + patrimonio:
-            tk.messagebox.showinfo("Balanço Patrimonial", "Balanço Patrimonial correto!")
-        else:
-            diferenca = ativo - (passivo + patrimonio)
-            tk.messagebox.showwarning("Balanço Patrimonial", f"Balanço Patrimonial incorreto!\nDiferença: {diferenca}")
+                print("Valor inválido. Tente novamente.\n")
+                continue
 
-root = tk.Tk()
-app = BalancoPatrimonialApp(master=root)
-app.mainloop()
+            itens.append((descricao, valor))
+            valor_total += valor
+            print(f"\n{descricao} adicionado com sucesso!\n")
+            
+            mais_itens = input(f"Deseja inserir outro item de {nome_categoria}?\n1: SIM\n2: NÃO\n> ")
+            if mais_itens.lower() == "1":
+                continue
+            elif mais_itens.lower() == "2":
+                print(f"\n{nome_categoria.title()} totais: {valor_total}\n")
+                if nome_categoria.lower() == "ativo":
+                    self.ativos = valor_total
+                elif nome_categoria.lower() == "passivo":
+                    self.passivos = valor_total
+                elif nome_categoria.lower() == "patrimônio líquido":
+                    self.patrimonio_liquido = valor_total
+                return itens, valor_total
+            else:
+                print("Resposta inválida. Tente novamente.\n")
+            
+    def calcular_balanco(self):
+        if self.ativos == self.passivos + self.patrimonio_liquido:
+            print("O balanço bateu! Ativo = Passivo + Patrimônio Líquido")
+        else:
+            print("O balanço não bateu! Verifique seus valores.")
+        
+        print("\nBalanço Patrimonial")
+        print(f"Ativos: R$ {self.ativos:.2f}")
+        print(f"Passivos: R$ {self.passivos:.2f}")
+        print(f"Patrimônio Líquido: R$ {self.patrimonio_liquido:.2f}")
+        
+        grau_endividamento = self.passivos / self.patrimonio_liquido
+        print(f"Grau de endividamento: {grau_endividamento:.2f}")
+
+bp = BalancoPatrimonial()
+bp.get_valor_total("Ativo")
+bp.get_valor_total("Passivo")
+bp.get_valor_total("Patrimônio Líquido")
+bp.calcular_balanco()
